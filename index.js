cf30dc0761332a08d6c63123b65426aeb4c61bcf
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2022-12-19 14:01:42 +0300

    C6

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ var myName;
 
 const quotient = 4.4 / 2.0; // Change this line
 const quotient2 = 4.4 / 2.0; // Change this line
+
+// Only change code below this line
+const increment = (number = 5, value = 2) => number + value;
+// Only change code above this line
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
