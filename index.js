a8fda98064c58589bd345900b3e5f7290cf96654
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-01-18 15:23:04 +0300

    C61

--- index.js
+++ index.js
@@ -293,3 +293,24 @@ class Vegetable {
 
 const carrot1 = new Vegetable('carrot');
 console.log(carrot1.name); // Should display 'carrot'
+
+// Only change code below this line
+class Thermostat{
+  constructor(temperatureF){
+    this._temperatureF = temperatureF;
+  }
+
+  get temperature() {
+    return 5/9 * (this._temperatureF - 32);
+  }
+
+  set temperature(temperatureC) {
+    this._temperatureF = temperatureC * 9/5 + 32;
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
