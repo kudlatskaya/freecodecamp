bef8b17477582342584220c18d6cf817bd96040c
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-01-18 13:30:32 +0300

    C56

--- index.js
+++ index.js
@@ -218,4 +218,13 @@ class Thermostat {
   let a = 8, b = 6;
   // Only change code below this line
    const [a, b] = [6, 8];
-   
+
+   function removeFirstTwo(list) {
+    // Only change code below this line
+    const [ ,,...shorterList] = list; // Change this line
+    // Only change code above this line
+    return shorterList;
+  }
+  
+  const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const sourceWithoutFirstTwo = removeFirstTwo(source);
