87161208aa846d182750b607b629e5336053bc10
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-01-18 13:20:36 +0300

    C54

--- index.js
+++ index.js
@@ -205,3 +205,14 @@ class Thermostat {
   
   // Only change code above this line
  
+  const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+  };
+  
+  // Only change code below this line
+    
+  const { today: { low:lowToday, high:highToday }, tomorrow: { low, high } } = LOCAL_FORECAST;
+  
+  // Only change code above this line
