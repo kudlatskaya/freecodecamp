1dd6189a6893674226fa6a4088d97f29206b2362
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-01-18 15:07:05 +0300

    C58

--- index.js
+++ index.js
@@ -241,3 +241,31 @@ class Thermostat {
   // Only change code below this line
   const half = ({max,min}) => (max + min) / 2.0; 
   // Only change code above this line
+
+  const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    // Only change code below this line
+    const failureItems = [];
+    for(let i = 0; i < arr.length; i++) {
+      failureItems[i] = `<li class="text-warning">${arr[i]}</li>`;
+    }
+      // Only change code above this line
+  
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure);
+
+  const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {
+      name,
+      age,
+      gender
+    };
+    // Only change code above this line
+  };
